f4862a5ca9366cd01f8bec1730ea61ccf1a9bb79
ComeOnProTM <101826779+ComeOnProTM@users.noreply.github.com>
2024-01-16 19:31:13 +0100

    Groovy I MainMenu
     - Main Menu implemented

---
+++ Assets/Game/Scripts/GroovyScene/MainMenu/MainMenuUI.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject containerMainMenu;
+    [SerializeField] private GameObject containerPartnerSelect;
+    [SerializeField] private Image background;
+    [SerializeField] private ButtonVR buttonPartnerSelect;
+    [SerializeField] private ButtonVR buttonQuit;
+    [SerializeField] private ButtonVR buttonDSH;
+    [SerializeField] private ButtonVR buttonPolice;
+    [SerializeField] private ButtonVR buttonUMCG;
+    [SerializeField] private ButtonVR buttonDUO;
+    [SerializeField] private ButtonVR buttonReturnMainMenu;
+
+    private void Awake()
+    {
+        containerMainMenu.SetActive(true);
+        containerPartnerSelect.SetActive(false);
+        background.gameObject.SetActive(true);
+    }
+
+    private void Start()
+    {
+        buttonPartnerSelect.OnClick += ButtonPartnerSelect_OnClick;
+        buttonQuit.OnClick += ButtonQuit_OnClick;
+        buttonDSH.OnClick += ButtonDSH_OnClick;
+        buttonPolice.OnClick += ButtonPolice_OnClick;
+        buttonUMCG.OnClick += ButtonUMCG_OnClick;
+        buttonDUO.OnClick += ButtonDUO_OnClick;
+        buttonReturnMainMenu.OnClick += ButtonReturnMainMenu_OnClick;
+    }
+
+    private void ButtonDUO_OnClick(object sender, ButtonVR.OnClickEventArgs e)
+    {
+        Debug.Log("Transfer to level");
+    }
+
+    private void ButtonUMCG_OnClick(object sender, ButtonVR.OnClickEventArgs e)
+    {
+        Debug.Log("Transfer to level");
+    }
+
+    private void ButtonPolice_OnClick(object sender, ButtonVR.OnClickEventArgs e)
+    {
+        Debug.Log("Transfer to level");
+    }
+
+    private void ButtonDSH_OnClick(object sender, ButtonVR.OnClickEventArgs e)
+    {
+        Debug.Log("Transfer to level");
+    }
+
+    private void ButtonQuit_OnClick(object sender, ButtonVR.OnClickEventArgs e)
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    private void ButtonReturnMainMenu_OnClick(object sender, ButtonVR.OnClickEventArgs e)
+    {
+        containerMainMenu.SetActive(true);
+        containerPartnerSelect.SetActive(false);
+        background.gameObject.SetActive(true);
+    }
+
+    private void ButtonPartnerSelect_OnClick(object sender, ButtonVR.OnClickEventArgs e)
+    {
+        containerMainMenu.SetActive(false);
+        containerPartnerSelect.SetActive(true);
+        background.gameObject.SetActive(true);
+    }
+}
